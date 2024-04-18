cfc85bc59165795d58598a9ce1e94d20c41b7bed
Deveshk10 <80586334+Deveshk10@users.noreply.github.com>
2024-04-18 17:36:35 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialsCards from "@/components/TestimonialsCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] anitaliased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialsCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer/>
+    </main>
+  );
+}
